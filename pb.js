845a12d6585093995ef722239238362a80144d12
jimenezale <cb.alejandra.jimenez.a@upds.net.bo>
2022-04-13 03:17:35 -0400

    main

---
+++ pb.js
@@ -0,0 +1,18 @@
+alert( 'Hola mundo');
+
+//variables
+let a=10;
+var b='Hola';
+var c=[1,2,3];
+
+//constante
+const pi=3.1416;
+var a1='$';
+var falso=false;
+var matriz=[[1,2,3],[3,2,1]];
+document.write
+{
+var nombre='Ale';
+var apellido='Astete'
+document.write(nombre+" "+apellido);
+}
