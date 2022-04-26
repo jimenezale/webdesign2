10a6b6eafc0667a63933320ecf5c1e26abf675b0
jimenezale <cb.alejandra.jimenez.a@upds.net.bo>
2022-04-26 16:45:21 -0400

    commit

--- pb.js
+++ pb.js
@@ -1,12 +1,12 @@
 alert( 'Bienvenido a mi portal de tareas');
 
 //variables
-let a=10;
+/*let a=10;
 var b='Hola';
 var c=[1,2,3];
-
+*/
 //constante
-const pi=3.1416;
+/*const pi=3.1416;
 var a1='$';
 var falso=false;
 var matriz=[[1,2,3],[3,2,1]];
@@ -15,4 +15,4 @@ document.write
 var nombre='Ale';
 var apellido='Astete'
 document.write(nombre+" "+apellido);
-}
+}*/
