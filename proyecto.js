3e0f28f177015e656e5ae1bad420fc27a001bede
jimenezale <cb.alejandra.jimenez.a@upds.net.bo>
2022-05-03 19:33:39 -0400

    commit

--- proyecto.js
+++ proyecto.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-    $("p").click(function(){
-      $(this).hide();
-    });
-  });
+var actual=1;
+$(document).on("ready",main);
+function main(){
+    var intervalo=setInterval("runcarrusel()",3000)
+  }
+  function runcarrusel(){
+      if(actual==$(".carrusel images").size())
+      {
+          actual=0;
+      }
+      $(".carrusel").animate({
+          marginLeft:(-1*actual*$(".carrusel image").eq(0).width())
+      },700);
+      actual++;
+  }
