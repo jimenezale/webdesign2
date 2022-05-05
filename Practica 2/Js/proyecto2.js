a52e716162aaece5a759206738d8275507c62eed
jimenezale <cb.alejandra.jimenez.a@upds.net.bo>
2022-05-05 16:26:19 -0400

    commit

---
+++ Practica 2/Js/proyecto2.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  $("#btn1-1").click(function () {
+    alert("Ingredientes para hacer Pollo a la portuguesa: *1 suprema de pollo * taza de tomate perita en lata *1 cebolla *½ ají morrón *1 zanahoria * ½ taza de arvejas congeladas *1 cucharadita de pimentón dulce *¼ cucharadita de pimienta *2 cucharadas soperas de harina o almidón de maíz"
+    );
+  });
+});
+$(document).ready(function () {
+  $("#btn2-1").click(function () {
+    alert("1  Empieza la receta de pollo a la portuguesa con papas pelando la cebolla y la zanahoria. Corta la cebolla y el ají en juliana,la zanahoria en rodajas.Saltea la cebolla con 1 cucharadita de aceite o aceite en aerosol y con el fuego alto, cuando se dore,baja el fuego a medio y agrega 1 cucharada de agua.Por otro lado, cuando se evapore el agua, agrega al ají y la zanahoria. Cada vez que se empiece a dorar, añade 1 cucharada de agua para que se cocine poco a poco, pero con el sabor del salteado. 2 Mientras las verduras se cocinan, corta la suprema en 3 porciones y pásalas por harina o maicena.De esta manera, se dorarán por fuera, generando una capa protectora para mantener la propia humedad de la carne.Por otro lado, cuando las verduras estén tan cocidas como desees, retíralas de la sartén y dora en el mismo fondo de cocción el pollo con la harina o con la maicena.No lo muevas hasta que esté dorado.Luego, dale la vuelta y déjalo que se dore por el otro lado.Señalamos que los muslos de pollo a la portuguesa también están deliciosos. 3  Cuando hayas terminado de dorar el pollo en la olla, agrega las verduras salteadas, los condimentos, las arvejas, los tomates perita pelados y la salsa de tomate. De esta forma, elaborarás una salsa portuguesa.Baja el fuego al mínimo y tapa la sartén.En 10 - 15 minutos el pollo estará cocido, ya podrás agregar un cuarto cucharadita de bicarbonato de sodio para que el tomate no esté ácido. Si estás preparando este plato con más tiempo, puedes cocinarlo por 40 minutos para que pierda la acidez y tome más sabor aún. ¡Ya casi tienes listo el pollo a la cacerola con papas! 4 Sirve tu pollo a la portuguesa tradicional con puré o papas al horno.  También puedes acompañarlo con arroz, fideos o batatas. Cuéntanos en los comentarios qué te ha parecido esta receta argentina.");
+  });
+});
+$(document).ready(function () {
+  $("#btn1-2").click(function () {
+    alert("INGREDIENTES PARA 2 PERSONAS: *1 cebolla *2 dientes de ajo *1 pimiento rojo *1 pimiento verde *3 o 4 huevos cocidos *1 huevo fresco *100ml de leche *1 tomate *100g de queso para fundir *40g de queso viejo *Orégano seco *Pimentón *Sal y pimienta *Aceite");
+  })
+});
+$(document).ready(function () {
+  $("#btn2-2").click(function () {
+    alert("COMO HACER HUEVOS AL ESTILO TURCO: En una sartén amplia, ponemos un chorrito de aceite y lo calentamos a temperatura media. Cuando esté caliente, añadimos los dientes de ajo, la cebolla, el pimiento rojo y el pimiento verde. Todos ellos cortados en cubos. Salpimentamos y dejamos cocinar alrededor de unos 15 minutos mientras removemos bien todos estos ingredientes. Pasado este tiempo, cuando la verdura esté blandita, la extendemos por toda la  superficie de la sartén.  Mientras tanto, seguimos con la elaboración de nuestros huevos turcos. En un bol, ponemos un  huevo y lo batimos brevemente. Añadimos 100ml de leche. Batimos nuevamente para que se integre la leche con el huevo. Añadimos esta mezcla en la sartén junto con las verduras y nos aseguramos que se extienda bien por toda la superfie de la sartén. A continuación, bajamos la potencia del fuego al mínimo y, sobre las verduras, ponemos unas rodajas de tomate entero, sin pelar. Las rodajas tiene que tener unos 2cm aproximadamente de grosor. Sobre ellas, añadimos un poco de queso rallado para fundir. A continuación, sobre este queso, ponemos el huevo cocido cortado en mitades, con la yema mirando hacia abajo. Hecho esto, espolvoreamos por toda la superficie de nuestros huevos turcos, orégano seco y pimentón (el pimentón puede ser dulce o picante, a tu elección). Ponemos nuevamente queso para fundir sobre toda la superficie y añadimos además un poco de queso curado- un queso viejo, fuerte, con sabor-. Tapamos la sartén y dejamos que se cocine todo durante unos 10 minutos a temperatura muy suave. Pasados estos 10 minutos, destapamos y espolvoreamos nuevamente con orégano. Servimos inmediatamente.");
+  })
+})
+$(document).ready(function () {
+  $("#btn1-3").click(function () {
+    alert("Ingredientes: Para preparar el caldo: Huesos de rodillas de ternera *1,5 kg. de pollo despiezado *Cebolleta china *150 g. setas shitake *30 g. atún seco en polvo o escamas, katsuobuchi *4 zanahorias *1 cda. semillas cilantro *1 cdita. granos pimienta negra *100 ml. salsa soja *100 ml. salsa de pescado thailandesa *100 ml. mirin *100 ml. sake *20 g. alga kobu --Acompañamientos: *6 rodajas lomo cerdo fresco *300 g. fideos para ramen *Cebollino chino, cebolleta *6 huevos");
+  });
+});
+$(document).ready(function () {
+  $("#btn2-3").click(function () {
+    alert("Base de la sopa En una cazuela ponemos 2 litros de agua y, cuando su temperatura llegue a los 65º C, añadimos las algas kombu. Mantenemos 30 minutos.Pasado el tiempo retiramos la mitad del agua colándola y reservándola. Volvemos a poner la cazuela al fuego, esta vez sólo con 1 litro de agua, y subimos la temperatura a 85ºC. Incorporamos el polvo de atún seco o las escamas de atún seco, dejamos reposar durante 5 minutos a 85ºC y retiramos la cazuela del fuego, reservamos,con el resto del agua de algas que tenemos reservada, volvemos a ponerla en una nueva cazuela al fuego.Llevamos el caldo a 40ºC, añadimos las setas shitake limpias y mantenemos a 40ºC durante 30 minutos. Pasado este tiempo retiramos la cazuela del fuego. En una olla exprés añadimos el caldo de setas, las propias setas shitake, el caldo de algas y atún seco previamente colado (desechamos las algas kombu), los huesos de rodilla, el pollo despiezado, las zanahorias, el cebollino chino o la cebolleta, los granos de pimienta, el cilantro, la sal, la salsa de soja, la salsa tailandesa de pescado, el mirin y el sake. El pollo lo añadiremos con toda su piel ya que dará mucho sabor al caldo. Una vez cocinado podremos desgrasarlo sin problemas, así que no nos preocuparemos a priori por la grasa que pueda tener el pollo. Cerramos la olla exprés y la ponemos al fuego.Cocinamos el caldo de nuestro ramen durante unas 2 horas. Pasado este tiempo dejamos que se enfríe y guardamos el caldo en el frigo. Preferentemente de un día para otro, para que la grasa suba a la superficie, se solidifique y podamos retirarla con facilidad a la mañana siguiente. Preparación del ramen. Presentación final Una vez que tenemos el caldo desgrasado y limpio lo volvemos a calentar ya que el ramen ha de servirse bien caliente. Mientras calentamos el caldo preparamos los ingredientes del ramen.Por un lado cocemos los huevos durante no más de 5 o 6 minutos. Enfriamos en agua fría para cortar la cocción,pelamos y los sumergimos en salsa de soja mientras preparamos el resto de ingredientes del plato.Cortamos la cebolleta china en rodajitas y reservamos.Cocemos los fideos chinos siguiendo las indicaciones del fabricante  y reservamos. Freímos ligeramente la carne de lomo de cerdo. Montamos el plato,servimos el caldo muy caliente del ramen. Añadimos en cada cuenco el resto de ingredientes. El huevo partido en dos,uno de los filetitos de lomo de cerdo, una parte de los fideos para ramen y espolvoreamos con cebolleta.");
+  });
+});
+$(document).ready(function () {
+  $("#btn1-4").click(function () {
+    alert("Ingredientes: *La carcasa de un pollo *1 cebolla *1 pimiento verde y/o rojo *1 puerro *1 o 2 zanahorias *Especias al gusto (para mí,la pimienta en grano, es obligatoria) *Sal *Vino blanco");
+  });
+});
+$(document).ready(function () {
+  $("#btn2-4").click(function () {
+    alert("Caldo de pollo: por Gorka Barredo    COMO HACER CALDO DE POLLO:Echamos todos los ingredientes en una olla u otro recipiente grande y llenamos de agua, hasta que los cubra dos o tres dedos por encima. Con los ingredientes ya dentro y cubiertos de agua, lo ponemos todo a calentar. Es importante empezar con el agua fría (temperatura ambiente) ya que así conseguiremos que nuestros ingredientes suelten más jugo que si los metiésemos en agua ya en ebullición. El motivo técnico de que esto sea así es que, si metemos los huesos en agua ya hirviendo, las proteínas de los huesos del pollo se coagulan y evitan que éstos suelten todo su jugo. Cuando comience a hervir el agua, bajamos el fuego y con una cuchara o cucharón,vamos eliminando la espuma que vaya saliendo de la superficie. Al principio, cuando el agua se caliente,saldrá mucha.Ésto,son las impurezas. Cuando no salga más espuma, lo tapamos y lo dejamos hervir a fuego suave durante, de una hora y media a tres horas, dependiendo de lo concentrado que queramos el caldo. Si lo hacemos en olla exprés, lo cual no recomiendo si eres principiante, con tenerlo de media a una hora a fuego suave será suficiente.Colamos el caldo y lo reservamos ya sin ingredientes sólidos. Lo metemos en el frigorífico durante una noche entera. Al día siguiente, habrá salido una especie de gelatina en la superficie. Esto es la grasa. Recomiendo eliminarla con una cuchara.");
+  });
+});
+$(document).ready(function () {
+  $("#id4").click(function () {
+    alert("Su receta fue enviada con exito"
+    );
+  });
+});
+$(document).ready(function(){
+    $("#d0").fadeIn();
+    $("#d1").fadeIn("slow");
+    $("#d2").fadeIn(2000);
+    $("#d3").fadeIn(3000);
+});
