a41576e7fe3048a164fe6366991f8bff31328579
jimenezale <cb.alejandra.jimenez.a@upds.net.bo>
2022-05-05 13:34:28 -0400

    commit

---
+++ proyecto1.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#d0").fadeIn();
+    $("#d1").fadeIn("slow");
+    $("#d2").fadeIn(2000);
+    $("#d3").fadeIn(3000);
+  });
--- proyecto2.js
+++ proyecto2.js
@@ -45,3 +45,9 @@ $(document).ready(function () {
     );
   });
 });
+$(document).ready(function(){
+    $("#d0").fadeIn();
+    $("#d1").fadeIn("slow");
+    $("#d2").fadeIn(2000);
+    $("#d3").fadeIn(3000);
+});
