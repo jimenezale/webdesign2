6e8cbc43189ed50f0ed11338d3a8ef821cafb570
jimenezale <cb.alejandra.jimenez.a@upds.net.bo>
2022-05-05 05:30:34 -0400

    commit

--- Practica 2/proyecto2.js
+++
@@ -1,3 +0,0 @@
-$("#btn1").click(function(){
-    $(.prep).hide();
-  });
---
+++ proyecto2.js
@@ -0,0 +1,12 @@
+$("#btn1").click(function () {
+  $(".ingr").show();
+});
+$("#btn2").click(function () {
+  $(".prep").show();
+});
+$("#btn1a").click(function () {
+  $(".ingr").hide();
+});
+$("#btn2a").click(function () {
+  $(".prep").hide();
+});
