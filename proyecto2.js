1d472a74d06c420f67ef4c36143960f00ae4362e
jimenezale <cb.alejandra.jimenez.a@upds.net.bo>
2022-05-05 10:28:22 -0400

    commit

--- proyecto2.js
+++ proyecto2.js
@@ -1,49 +1,56 @@
-$("#btn1-1").click(function () {
-  $(".ingra").show();
+$(document).ready(function () {
+  $("#btn1-1").click(function () {
+    alert("Ingredientes para hacer Pollo a la portuguesa: *1 suprema de pollo * taza de tomate perita en lata *1 cebolla *½ ají morrón *1 zanahoria * ½ taza de arvejas congeladas *1 cucharadita de pimentón dulce *¼ cucharadita de pimienta *2 cucharadas soperas de harina o almidón de maíz"
+    );
+  });
 });
-$("#btn2-1").click(function () {
-  $(".prepa").show();
+$(document).ready(function () {
+  $("#btn2-1").click(function () {
+    alert("1  Empieza la receta de pollo a la portuguesa con papas pelando la cebolla y la zanahoria. Corta la cebolla y el ají en juliana,la zanahoria en rodajas.Saltea la cebolla con 1 cucharadita de aceite o aceite en aerosol y con el fuego alto, cuando se dore,baja el fuego a medio y agrega 1 cucharada de agua.Por otro lado, cuando se evapore el agua, agrega al ají y la zanahoria. Cada vez que se empiece a dorar, añade 1 cucharada de agua para que se cocine poco a poco, pero con el sabor del salteado. 2 Mientras las verduras se cocinan, corta la suprema en 3 porciones y pásalas por harina o maicena.De esta manera, se dorarán por fuera, generando una capa protectora para mantener la propia humedad de la carne.Por otro lado, cuando las verduras estén tan cocidas como desees, retíralas de la sartén y dora en el mismo fondo de cocción el pollo con la harina o con la maicena.No lo muevas hasta que esté dorado.Luego, dale la vuelta y déjalo que se dore por el otro lado.Señalamos que los muslos de pollo a la portuguesa también están deliciosos. 3  Cuando hayas terminado de dorar el pollo en la olla, agrega las verduras salteadas, los condimentos, las arvejas, los tomates perita pelados y la salsa de tomate. De esta forma, elaborarás una salsa portuguesa.Baja el fuego al mínimo y tapa la sartén.En 10 - 15 minutos el pollo estará cocido, ya podrás agregar un cuarto cucharadita de bicarbonato de sodio para que el tomate no esté ácido. Si estás preparando este plato con más tiempo, puedes cocinarlo por 40 minutos para que pierda la acidez y tome más sabor aún. ¡Ya casi tienes listo el pollo a la cacerola con papas! 4 Sirve tu pollo a la portuguesa tradicional con puré o papas al horno.  También puedes acompañarlo con arroz, fideos o batatas. Cuéntanos en los comentarios qué te ha parecido esta receta argentina.");
+  });
 });
-$("#btn1a").click(function () {
-  $(".ingra").hide();
+          $("#btn2a").click(function () {
+            $(".prepa").hide();
 });
-$("#btn2a").click(function () {
-  $(".prepa").hide();
+          $("#btn1-2").click(function () {
+            $(".ingrb").show();
 });
-$("#btn1-2").click(function () {
-  $(".ingrb").show();
+          $("#btn2-2").click(function () {
+            $(".prepb").show();
 });
-$("#btn2-2").click(function () {
-  $(".prepb").show();
+          $("#btn1b").click(function () {
+            $(".ingrb").hide();
 });
-$("#btn1b").click(function () {
-  $(".ingrb").hide();
+          $("#btn2b").click(function () {
+            $(".prepb").hide();
 });
-$("#btn2b").click(function () {
-  $(".prepb").hide();
+          $("#btn1-3").click(function () {
+            $(".ingrc").show();
 });
-$("#btn1-3").click(function () {
-  $(".ingrc").show();
+          $("#btn2-3").click(function () {
+            $(".prepc").show();
 });
-$("#btn2-3").click(function () {
-  $(".prepc").show();
+          $("#btn1c").click(function () {
+            $(".ingrc").hide();
 });
-$("#btn1c").click(function () {
-  $(".ingrc").hide();
+          $("#btn2c").click(function () {
+            $(".prepc").hide();
 });
-$("#btn2c").click(function () {
-  $(".prepc").hide();
+          $("#btn1-4").click(function () {
+            $(".ingrd").show();
 });
-$("#btn1-4").click(function () {
-  $(".ingrd").show();
+          $("#btn2-4").click(function () {
+            $(".prepd").show();
 });
-$("#btn2-4").click(function () {
-  $(".prepd").show();
+          $("#btn1d").click(function () {
+            $(".ingrd").hide();
 });
-$("#btn1d").click(function () {
-  $(".ingrd").hide();
+          $("#btn2d").click(function () {
+            $(".prepd").hide();
 });
-$("#btn2d").click(function () {
-  $(".prepd").hide();
+          $(document).ready(function(){
+            $("#id4").click(function () {
+              alert("Su receta fue enviada con exito"
+              );
+            });
 });
-
