2d2df7dc7d2c6f9c96f3dadfdb1dfb30b692be0c
jimenezale <cb.alejandra.jimenez.a@upds.net.bo>
2022-05-05 09:21:24 -0400

    commit

--- proyecto2.js
+++ proyecto2.js
@@ -1,13 +1,49 @@
-$("#btn1").click(function () {
-  $(".ingr").show();
+$("#btn1-1").click(function () {
+  $(".ingra").show();
 });
-$("#btn2").click(function () {
-  $(".prep").show();
+$("#btn2-1").click(function () {
+  $(".prepa").show();
 });
 $("#btn1a").click(function () {
-  $(".ingr").hide();
+  $(".ingra").hide();
 });
 $("#btn2a").click(function () {
-  $(".prep").hide();
+  $(".prepa").hide();
+});
+$("#btn1-2").click(function () {
+  $(".ingrb").show();
+});
+$("#btn2-2").click(function () {
+  $(".prepb").show();
+});
+$("#btn1b").click(function () {
+  $(".ingrb").hide();
+});
+$("#btn2b").click(function () {
+  $(".prepb").hide();
+});
+$("#btn1-3").click(function () {
+  $(".ingrc").show();
+});
+$("#btn2-3").click(function () {
+  $(".prepc").show();
+});
+$("#btn1c").click(function () {
+  $(".ingrc").hide();
+});
+$("#btn2c").click(function () {
+  $(".prepc").hide();
+});
+$("#btn1-4").click(function () {
+  $(".ingrd").show();
+});
+$("#btn2-4").click(function () {
+  $(".prepd").show();
+});
+$("#btn1d").click(function () {
+  $(".ingrd").hide();
+});
+$("#btn2d").click(function () {
+  $(".prepd").hide();
 });
 
